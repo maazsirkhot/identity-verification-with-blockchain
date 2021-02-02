3329b708bdc725be774d95e808f6daffc68099dd
ihetal <hetal.shah@sjsu.edu>
2021-02-02 12:17:15 -0800

    Completing all home page changes

--- frontend/src/components/Homepage/Home.js
+++ frontend/src/components/Homepage/Home.js
@@ -8,6 +8,7 @@ import features2 from "../../assets/img/features2.jpg";
 // import "../../assets/css/test.css";
 
 // import "../../assets/css/boot.css";
+// import "../../assets/css/nine.css";
 
 export class Home extends Component {
   render() {
@@ -19,9 +20,9 @@ export class Home extends Component {
               <g
                 id="Page-1"
                 stroke="none"
-                stroke-width="1"
+                strokeWidth="1"
                 fill="none"
-                fill-rule="evenodd"
+                fillRule="evenodd"
               >
                 <g
                   id="Apple-TV"
@@ -165,67 +166,182 @@ export class Home extends Component {
               </div>
             </div>
           </div>
-          <br />
-          <br />
         </section>
         <section class="features">
-          <div class="">
-            <div class="row align-items-center" style={{ margin: "50px" }}>
-              <div class="col-md-6 mr-auto">
-                <h2 class="mb-4">Access Control</h2>
+          <div class="row align-items-center" style={{ margin: "50px" }}>
+            <div class="col-md-6 mr-auto">
+              <h2 class="mb-4">Access Control</h2>
 
-                <ul data-aos="zoom-in">
-                  <li>
-                    <i class="fas fa-check"></i> Users manage and control who
-                    sees what about them with one click
-                  </li>
-                  <li>
-                    <i class="fas fa-check"></i> Secure storage of your data
-                    using blockchain
-                  </li>
-                  <li>
-                    <i class="fas fa-check"></i>
-                    Personal information hidden from requestor and verified
-                    using zero-knowledge proof
-                  </li>
-                  <li>
-                    <i class="fas fa-check"></i> Detailed view of verification
-                    results
-                  </li>
-                </ul>
-              </div>
-              <div class="col-md-4" data-aos="fade-left">
-                <img src={features1} alt="" class="img-fluid" />
-              </div>
+              <ul data-aos="zoom-in">
+                <li>
+                  <i class="fas fa-check"></i> Users manage and control who sees
+                  what about them with one click
+                </li>
+                <li>
+                  <i class="fas fa-check"></i> Secure storage of your data using
+                  blockchain
+                </li>
+                <li>
+                  <i class="fas fa-check"></i>
+                  Personal information hidden from requestor and verified using
+                  zero-knowledge proof
+                </li>
+                <li>
+                  <i class="fas fa-check"></i> Detailed view of verification
+                  results
+                </li>
+              </ul>
             </div>
-            <div class="row align-items-center">
-              <div class="col-md-6" data-aos="fade-right">
-                <img src={features2} alt="" class="img-fluid" />
-              </div>
-              <div class="col-md-5 mr-auto">
-                <h2 class="mb-4">
-                  Seamless Integration with Business Ecosystem
-                </h2>
+            <div class="col-md-4" data-aos="fade-left">
+              <img src={features1} alt="" class="img-fluid" />
+            </div>
+          </div>
+          <div class="row align-items-center">
+            <div class="col-md-6" data-aos="fade-right">
+              <img src={features2} alt="" class="img-fluid" />
+            </div>
+            <div class="col-md-5 mr-auto">
+              <h2 class="mb-4">Seamless Integration with Business Ecosystem</h2>
 
-                <ul data-aos="zoom-in">
-                  <li>
-                    <i class="fas fa-check"></i> Cloud based with fast setup and
-                    easy configuration
-                  </li>
-                  <li>
-                    <i class="fas fa-check"></i> Send a link to client and get
-                    verification results instantly in your Admin Panel
-                  </li>
-                  <li>
-                    <i class="fas fa-check"></i>
-                    Integrate client verification solutions into your platform,
-                    collect data you need and get the results via API.
-                  </li>
-                </ul>
+              <ul data-aos="zoom-in">
+                <li>
+                  <i class="fas fa-check"></i> Cloud based with fast setup and
+                  easy configuration
+                </li>
+                <li>
+                  <i class="fas fa-check"></i> Send a link to client and get
+                  verification results instantly in your Admin Panel
+                </li>
+                <li>
+                  <i class="fas fa-check"></i>
+                  Integrate client verification solutions into your platform,
+                  collect data you need and get the results via API.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <br />
+        <br />
+        <section id="contact" class="contact" style={{ marginBottom: "15px" }}>
+          <div class="container">
+            <div class="section-title" data-aos="fade-up">
+              <h2>Contact us to get started</h2>
+            </div>
+
+            <div class="row">
+              <div
+                class="col-lg-5 d-flex align-items-stretch"
+                data-aos="fade-up"
+                data-aos-delay="100"
+              >
+                <div class="info">
+                  <div class="address">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <h4>Location:</h4>
+                    <p>1 Washington Sq, San Jose, CA 95192</p>
+                  </div>
+
+                  <div class="email">
+                    <i class="fas fa-envelope"></i>
+                    <h4>Email:</h4>
+                    <p>verify@sjsu.edu</p>
+                  </div>
+
+                  <div class="phone">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <h4>Call:</h4>
+                    <p>+1 123 456 7890</p>
+                  </div>
+
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.3092217614085!2d-121.88326018473684!3d37.33518737984176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fccb864de43d5%3A0x397ffe721937340e!2sSan%20Jos%C3%A9%20State%20University!5e0!3m2!1sen!2sus!4v1612294981924!5m2!1sen!2sus"
+                    frameBorder="0"
+                    style={{ border: "0", width: "100%", height: "290px" }}
+                    allowFullScreen=""
+                    title="Location"
+                  ></iframe>
+                </div>
+              </div>
+
+              <div
+                class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch"
+                data-aos="fade-up"
+                data-aos-delay="200"
+              >
+                <form class="php-email-form">
+                  <div class="form-row">
+                    <div class="form-group col-md-6">
+                      <label>Your Name</label>
+                      <input
+                        type="text"
+                        name="name"
+                        class="form-control"
+                        id="name"
+                        data-rule="minlen:4"
+                        data-msg="Please enter at least 4 chars"
+                      />
+                      <div class="validate"></div>
+                    </div>
+                    <div class="form-group col-md-6">
+                      <label>Your Email</label>
+                      <input
+                        type="email"
+                        class="form-control"
+                        name="email"
+                        id="email"
+                        data-rule="email"
+                        data-msg="Please enter a valid email"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label>Subject</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      name="subject"
+                      id="subject"
+                      data-rule="minlen:4"
+                      data-msg="Please enter at least 8 chars of subject"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label>Message</label>
+                    <textarea
+                      class="form-control"
+                      name="message"
+                      rows="10"
+                      data-rule="required"
+                      data-msg="Please write something for us"
+                    ></textarea>
+                  </div>
+
+                  <div class="text-center">
+                    <button type="submit">Send Message</button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </section>
+        <br />
+        <br />
+        <footer id="footer">
+          <div class="footer-newsletter" data-aos="fade-up">
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-6">
+                  <h4>Join Our Newsletter</h4>
+                  <form action="" method="post">
+                    <input type="email" name="email" />
+                    <input type="submit" value="Subscribe" />
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
