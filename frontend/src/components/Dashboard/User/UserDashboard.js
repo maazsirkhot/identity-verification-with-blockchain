f90ca5dc17a65344df9cacf0370e6ed830701f1d
ihetal <hetal.shah@sjsu.edu>
2021-02-05 22:57:45 -0800

    Dashboard Header

---
+++ frontend/src/components/Dashboard/User/UserDashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import DashboardNavbar from "../../Header/DashboardNavbar";
+
+export class UserDashboard extends Component {
+  render() {
+    return (
+      <div class="main-wrapper">
+        <DashboardNavbar />
+      </div>
+    );
+  }
+}
+
+export default UserDashboard;
