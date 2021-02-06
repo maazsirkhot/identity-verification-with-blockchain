6ff274d5f1d64d649eba0755ab3fd0146954d926
ihetal <hetal.shah@sjsu.edu>
2021-02-05 23:49:50 -0800

    Dashboard Sidebar

--- frontend/src/components/Dashboard/User/UserDashboard.js
+++ frontend/src/components/Dashboard/User/UserDashboard.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import DashboardNavbar from "../../Header/DashboardNavbar";
-
+import SideBar from "../../Header/SideBar";
+import "../../../assets/css/header.css";
+import "../../../assets/css/dashboard.css";
 export class UserDashboard extends Component {
   render() {
     return (
       <div class="main-wrapper">
         <DashboardNavbar />
+        <SideBar />
       </div>
     );
   }
