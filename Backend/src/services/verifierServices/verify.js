5d3e1a6fc0e1405c6ecaf6d79932f4f27f4a3878
shraddhanayak07 <shraddhanayak07@gmail.com>
2021-04-19 08:49:54 -0700

    Blockchain integration with the Backend

--- Backend/src/services/verifierServices/verify.js
+++ Backend/src/services/verifierServices/verify.js
@@ -1,6 +1,8 @@
 const verifier = require('../../daos/userFields/userFields');
 const verificationEntity = require('../../daos/verificationEntity/verificationEntity');
 const _ = require('lodash');
+const fetch = require('node-fetch');
+const constants = require('../../../utils/constants');
 
 module.exports = {
   getAllUsersByVerifierDoc: async (idType) => {
@@ -26,11 +28,36 @@ module.exports = {
       throw new Error(`Error Occurred in Service Layers: ${error}`);
     }
   },
-  updateUserData: async (userDetails, verifierId) => {
+  updateUserData: async (userDetails, verifierId, walletId) => {
     try {
       userDetails.verifierApproval.verifiedBy = verifierId;
+      const dataField = userDetails.dataField;
+      _.each(dataField, (currentData) => {
+        currentData['dataReference'] = walletId;
+      });
       return await verifier.updateUserData(userDetails);
     } catch (error) {
+      throw new Error(`Error Occurred in Service Layers: ${error}`);
+    }
+  },
+  getWalletIdFromBlockchainService: async (userId, verifierId) => {
+    try {
+      const verfierData = await verificationEntity.findVerifierById(verifierId);
+      const blockchainData = {
+        userId,
+        docType: verfierData.idtype,
+        verifier: verfierData.idtype == 'CADL'?'DL Authority':'Passport Authority',
+      }
+      
+      const blockchainResponseData = await fetch(constants.ENV_VARIABLES.BLOCKCHAIN_HOST + '/resources', {method: 'POST', body: blockchainData});
+      const blockchainResponseJson = await blockchainResponseData.json();
+
+      if (blockchainResponseJson.status == '200')
+        return blockchainResponseJson.walletId;
+      else
+        return false;
+    } catch (error) {
+      console.log(error);
       throw new Error(`Error Occurred in Service Layers: ${error}`);
     }
   },
